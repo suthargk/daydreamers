66cfaf646aa1c7caef56e800621742916d6fce76
suthargk <gaurav.suthar161@gmail.com>
2021-03-25 21:51:20 +0530

    Added: founder, sponsor and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,14 @@ module.exports = {
       colors: {
         primary: "#7510f7",
         primaryLight: "#6e01f2",
+        darkGrayBlue: "#141c3a",
+      },
+      fontFamily: {
+        nanum: ["Nanum Pen Script"],
       },
       backgroundImage: (theme) => ({
         "event-card-image": "url('/public/img/picture_1.jpg')",
+        // "hero-image": "url('/public/img/hero.jpg')",
       }),
     },
   },
